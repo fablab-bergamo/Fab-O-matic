d8cd178c1b58d8d04391de1776f3a78a5f4428bb
Pascal Brunot <pascal.brunot@gmail.com>
2024-12-21 10:47:57 +0100

    Use stronger types

--- include/OTA.hpp
+++ include/OTA.hpp
@@ -12,11 +12,20 @@ const char *const password = fabomatic::secrets::credentials::password.data();
 
 namespace fabomatic
 {
+  enum struct ForceReset
+  {
+    True,
+    False
+  };
+  enum struct DisablePortal { 
+    True,
+    False 
+  };
 
   /// @brief Open the ArduinoOTA configuration portal
   /// @param force_reset true to discard saved settings and restore compile-time settings
   /// @param disable_portal true to skip portal opening (used at boot time)
-  auto openConfigPortal(bool force_reset, bool disable_portal) -> void;
+  auto openConfigPortal(ForceReset force_reset, DisablePortal disable_portal) -> void;
 
   /// @brief Initialization of the Arduino OTA library with settings for fabomatic
   auto setupOTA() -> void;
